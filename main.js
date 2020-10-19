6cd7bcbb86161db3fe9c72ad77e16f5cd7f847c3
Gregory.Johnson <3531op@gmail.com>
2020-10-19 15:00:01 -0400

    Initial commit.

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 // Your code below.
+const makeDino = function (species, period, carnivore, extinct) {
+  return {
+    species: species,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct || false
+  }
+}
 
+const makeExtinct = function (obj) {
+  return makeDino(obj.species, obj.period, obj.carnivore, true)
+}
 
+const truncateSpecies = function (obj) {
+  if (obj.species.length > 10) {
+    return makeDino(obj.species.slice(0, 7) + "...", obj.period, obj.carnivore, obj.extinct)
+  } else {
+    return makeDino(obj.species, obj.period, obj.carnivore, obj.extinct)
+  }
+}
 
-
+const makeSingular = function (obj) {
+  if (obj.species.endsWith('us')) {
+    return makeDino(obj.species.slice(0, -2), obj.period, obj.carnivore, obj.extinct)
+  } else {
+    return makeDino(obj.species, obj.period, obj.carnivore, obj.extinct)
+  }
+}
 
 // Our code below; DO NOT TOUCH!
 if (typeof makeDino === 'undefined') {
